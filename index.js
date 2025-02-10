43245b7051b69011018047da181d789479273738
victorrobles-dev <roblesdocarmo@gmail.com>
2025-02-10 18:53:59 -0300

    desafio felipão concluído

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-let nome = "Victor"
-console.log("Olá mundooo")
-console.log("Olá, " + nome)
+let nomeHeroi = 'Gandalf o "Cinzento"'
+let xp = 8000
+
+if (xp < 1000) {
+    console.log("O herói " + nomeHeroi + " está no ranking Ferro")
+} 
+else if (xp >= 1001 && xp <= 2000) {
+    console.log("O herói " + nomeHeroi + " está no ranking Bronze")
+}
+else if (xp >= 2001 && xp <= 5000) {
+    console.log("O herói " + nomeHeroi + " está no ranking Prata")
+}
+else if (xp >= 5001 && xp <= 7000) { 
+    console.log("O herói " + nomeHeroi + " está no ranking Ouro")
+}
+else if (xp >= 7001 && xp <= 8000) {
+    console.log("O herói " + nomeHeroi + " está no ranking Platina")
+}
+else if (xp >= 8001 && xp <= 9000) {
+    console.log("O herói " + nomeHeroi + " está no ranking Ascendente")
+}
+else if (xp >= 9001 && xp <= 10000) {
+    console.log("O herói " + nomeHeroi + " está no ranking Imortal")
+}
+else if (xp >= 10001) {
+    console.log("O herói " + nomeHeroi + " está no ranking Radiante!")
+}
